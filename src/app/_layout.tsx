d4ba17025ed9d7695d225eb10e84da4da64273e1
vk-vasanth-vk <vasanthakumar.devops@gmail.com>
2026-06-16 21:53:46 +0530

    Configure project for Expo SDK 54

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,4 +1,4 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useColorScheme } from 'react-native';
 
 import { AnimatedSplashOverlay } from '@/components/animated-icon';
